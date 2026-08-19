87fb0b67300e1f674cc41b08b565ff1aa7517397
Nate H <nathan.hertner@gmail.com>
2026-08-18 21:12:46 -0400

    avatar upload work

--- server.js
+++ server.js
@@ -8,17 +8,88 @@ import genericRouter from './routes/generic.router.js';
 import demoRouter from './routes/demo.router.js';
 import pickemsRouter from './routes/pickems.router.js';
 import { httpLogger } from './logging/requestLoggingHandler.js'
+// image upload related
+import path from 'node:path';
+import multer from 'multer';
+import * as fs from 'node:fs';
+import sharp from 'sharp';
 
 const PORT = 5000;
 const app = express();
-
-// Log all traffic in the requests.log file
-app.use(httpLogger);
+const __dirname = import.meta.dirname;
 
 // node.js express features
 app.use(express.json()); // Parses incoming JSON payloads
 app.use(express.urlencoded({ extended: true })); // Parses URL-encoded data (from HTML forms)
 app.use(cors()); // allow cors communication
+
+// Log all traffic in the requests.log file
+app.use(httpLogger);
+
+// File handling
+const uploadDir = path.join(__dirname, 'avatar_uploads');
+if (!fs.existsSync(uploadDir)) {
+    fs.mkdirSync(uploadDir);
+}
+
+// Configure Multer disk storage
+// const storage = multer.diskStorage({
+//     destination: (req, file, cb) => {
+//         cb(null, 'avatar_uploads/');
+//     },
+//     filename: (req, file, cb) => {
+//         const defaultName = Date.now() + '-' + Math.round(Math.random() * 1E9);
+//         const customName = req.params.email || defaultName;
+//         const ext = path.extname(file.originalname);
+
+//         cb(null, `${customName}${ext}`);
+//     }
+// });
+
+const upload = multer({
+    storage: multer.memoryStorage(),
+    limits: { fileSize: 5 * 1024 * 1024 }, // Limit to 5MB
+    fileFilter: (req, file, cb) => {
+        if (file.mimetype.startsWith('image/')) {
+            cb(null, true);
+        } else {
+            cb(new Error('Only image files are allowed!'), false);
+        }
+    }
+});
+
+// The route string 'image' must match the Angular FormData key
+app.post('/api/upload_avatar/:email', upload.single('image'), async (req, res) => {
+    try {
+
+        const email = req.params.email;
+        if (!email) {
+            return res.status(400).json({ error: 'Missing email parameter' });
+        }
+        if (!req.file) {
+            return res.status(400).json({ message: 'No file uploaded' });
+        }
+
+        // Set avatar filename to user email .png
+        const finalFilename = `${email}.png`;
+        const outputPath = path.join(uploadDir, finalFilename);
+
+        await sharp(req.file.buffer)
+            .png({
+                quality: 90,
+                compressionLevel: 6,
+                effort: 1
+            }) // Convert & optimize to PNG format
+            .toFile(outputPath);
+
+        res.status(200).json({
+            message: 'Avatar saved successfully!'
+        });
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Server error while processing image');
+    }
+});
 
 // api routes
 app.use('/api/', genericRouter);
